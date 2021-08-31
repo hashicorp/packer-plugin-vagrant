9d8bf4b8bdbe38452a2fa2b0a603d2b03068b2e8
Adrien Delorme <azr@users.noreply.github.com>
2021-08-31 16:00:39 +0200

    consolidate version location

--- main.go
+++ main.go
@@ -4,26 +4,12 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/hashicorp/packer-plugin-sdk/plugin"
+
 	vagrantB "github.com/hashicorp/packer-plugin-vagrant/builder/vagrant"
 	vagrantPP "github.com/hashicorp/packer-plugin-vagrant/post-processor/vagrant"
 	vagrantCloudPP "github.com/hashicorp/packer-plugin-vagrant/post-processor/vagrant-cloud"
-
-	"github.com/hashicorp/packer-plugin-sdk/plugin"
-	"github.com/hashicorp/packer-plugin-sdk/version"
-)
-
-var (
-	// Version is the main version number that is being run at the moment.
-	Version = "1.0.0"
-
-	// VersionPrerelease is A pre-release marker for the Version. If this is ""
-	// (empty string) then it means that it is a final release. Otherwise, this
-	// is a pre-release such as "dev" (in development), "beta", "rc1", etc.
-	VersionPrerelease = ""
-
-	// PluginVersion is used by the plugin set to allow Packer to recognize
-	// what version this plugin is.
-	PluginVersion = version.InitializePluginVersion(Version, VersionPrerelease)
+	"github.com/hashicorp/packer-plugin-vagrant/version"
 )
 
 func main() {
@@ -31,7 +17,7 @@ func main() {
 	pps.RegisterBuilder(plugin.DEFAULT_NAME, new(vagrantB.Builder))
 	pps.RegisterPostProcessor(plugin.DEFAULT_NAME, new(vagrantPP.PostProcessor))
 	pps.RegisterPostProcessor("cloud", new(vagrantCloudPP.PostProcessor))
-	pps.SetVersion(PluginVersion)
+	pps.SetVersion(version.PluginVersion)
 	err := pps.Run()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
